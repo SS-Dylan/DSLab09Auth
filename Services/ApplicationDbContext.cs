9ceddc747ce30c2359657315b48034da8b3e6ee1
Dylan Shaffer <13596307+WRX-Dylan@users.noreply.github.com>
2022-11-09 01:12:54 -0500

    Add project files.

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DSLab09Auth.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSLab09Auth.Services;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
